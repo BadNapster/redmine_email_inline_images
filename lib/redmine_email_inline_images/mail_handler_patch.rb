8769d5eccb5deb16ef517d1c2613cc58ee09beb5
BadNapster <kirill.saveliev@gmail.com>
2022-04-26 16:57:01 +0300

    Add issue reply support

--- lib/redmine_email_inline_images/mail_handler_patch.rb
+++ lib/redmine_email_inline_images/mail_handler_patch.rb
@@ -59,27 +59,49 @@ module RedmineEmailInlineImages
       # to prevent overlapping names when replies come in
       def add_attachments_with_inline_images(obj)
         add_attachments_default(obj)
-        obj.reload
 
-        # route/path to attachments
-        # need to get this from redmine installation
-        path = "/attachments/download"
-
-        obj.attachments.each do |att|
-          if @images.has_value?(att.filename)
+        if obj.is_a?(Issue)
+          # Rails.logger.info "InlineMailHandler: issue ##{obj.id} passed"  
+          # route/path to attachments
+          # need to get this from redmine installation
+          path = "/attachments/download"
+          
+          obj.reload
+          last_journal_id = obj.last_journal_id
+          if last_journal_id.nil?
+            description = obj.description            
+            # Rails.logger.info "InlineMailHandler: no last_journal find"
+          else
+            last_journal = Journal.find(last_journal_id) 
+            # Rails.logger.info "InlineMailHandler: last_journal ##{last_journal.id} find"
+            description = last_journal.notes
+          end                  
+                   
+          obj.attachments.each do |att|
             str_r = Regexp.escape(att.filename)
             str_r = "\\[(?:image|cid).?:.?#{str_r}(?:[@A-Z\\d\\.]*)\\]"
             regex = Regexp.new(str_r)
-            obj.description.scan(regex).each do |match|
-              # tmp_desc = obj.description.gsub(match, "#{@strOpen}#{path}/#{att.id}/#{att.filename}#{@strClose}")
-              tmp_desc = obj.description.gsub(match, "#{@strOpen}#{att.filename}#{@strClose}")
-              obj.description = tmp_desc
+            if @images.has_value?(att.filename)              
+              description.scan(regex).each do |match|
+                tmp_desc = description.gsub(match, "#{@strOpen}#{path}/#{att.id}/#{att.filename}#{@strClose}")
+                # tmp_desc = description.gsub(match, "#{@strOpen}#{att.filename}#{@strClose}")
+                description = tmp_desc
+              end        
+              # Rails.logger.info "InlineMailHandler: description ##{description}"
             end
           end
-        end
-
-        obj.save!
-      end      
+          
+          if last_journal_id.nil? 
+            # Rails.logger.info "InlineMailHandler: obj.save!"
+            obj.description = description
+            obj.save!
+          else            
+            # Rails.logger.info "InlineMailHandler: last_journal.save"
+            last_journal.notes = description
+            last_journal.save
+          end          
+        end        
+      end 
     end # module InstanceMethods
   end # module MailHandlerPatch
 end # module RedmineEmailInlineImages
